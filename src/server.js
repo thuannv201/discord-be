dc21f200bc65123b25ce48f8cc1b31557348572c
hochv <hoc17112001@gmail.com>
2022-07-19 16:13:37 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const db = require("./config");
+const cors = require("cors");
 const routes = require("./routes");
 const app = express();
+app.use(
+  // yarn add cors
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+app.options("*", cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 const PORT = process.env.PORT || 4508
