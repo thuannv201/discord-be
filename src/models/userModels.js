38ec790247bd395d936eb449cd2eeebfeff7975e
kienpm <manhkien1998@gmail.com>
2022-07-29 17:17:34 +0700

    fix

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -1,18 +1,51 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema(
-  {
-    username: {type: String, unique: true, default: "", maxlength: 50},
-    password: {type: String, default: 123, maxlength: 250},
-    role: {type: String, default: "init"},
-    isActive: {type: Boolean, default: false}, //false until 
-    email: {type: String, default: "hoc17112001@gmail.com", unique:true},
-    phone: {type: String, default: ""},
-  },
-  {
-    timestamps: true,
-  }
+	{
+		username: {
+			type: String,
+			unique: true,
+			default: '',
+			maxlength: 50,
+			validate: {
+				validator: function (v) {
+					return v.length >= 6;
+				},
+				message: (props) => `Must be at least 6, got ${props.value.length}`,
+			},
+			required: [true, 'Username is required'],
+		},
+		email: {
+			type: String,
+			validate: {
+				validator: function (v) {
+					const regEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+					return regEmail.test(v);
+				},
+				message: () => 'Invalid email',
+			},
+			required: [true, 'Email is required'],
+
+			unique: true,
+		},
+		password: {
+			type: String,
+			validate: {
+				validator: function (v) {
+					return v.length >= 6;
+				},
+				message: (props) => `Must be at least 6, got ${props.value.length}`,
+			},
+			required: [true, 'Password is required'],
+		},
+		role: { type: String, default: 'init' },
+		isActive: { type: Boolean, default: false }, //false until
+		phone: { type: String, default: '' },
+	},
+	{
+		timestamps: true,
+	}
 );
 
-module.exports = mongoose.model("Model", User, "user");
+module.exports = mongoose.model('Model', User, 'user');
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,12 +1,17 @@
-const express = require("express");
-const authController = require("../../controller/authen");
-const {verifyToken, verifyUser, validateUsername, verifyTokenResetPw} = require("../../middleware");
+const express = require('express');
+const authController = require('../../controller/authen');
+const {
+	verifyToken,
+	verifyUser,
+	validateUsername,
+	verifyTokenResetPw,
+} = require('../../middleware');
 const router = express.Router();
-router.post("/forgotPassword", authController.forgotPW);
-router.get("/test", verifyToken, authController.test);
-router.post("/login", verifyUser, authController.resolveLogin);
-router.post("/refreshToken", authController.refreshToken);
-router.post("/register", validateUsername, authController.register);
-router.post("/reset", verifyTokenResetPw, authController.reset);
+router.post('/forgotPassword', authController.forgotPW);
+router.get('/test', verifyToken, authController.test);
+router.post('/login', verifyUser, authController.resolveLogin);
+router.post('/refreshToken', authController.refresh);
+router.post('/register', validateUsername, authController.register);
+router.post('/reset', verifyTokenResetPw, authController.reset);
 
 module.exports = router;
