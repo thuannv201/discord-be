fd8741c25fa93a06298d6ca78c6248743e381472
Hoang Van Hoc <hochv@i.hivetech.vn>
2022-08-09 22:39:27 +0700

    ok

--- src/controller/user/personal.js
+++ src/controller/user/personal.js
@@ -1,20 +1,24 @@
 const UserModel = require("../../models/user/userModels");
 const ServerModel = require("../../models/servers/server");
-const {sendSuccessMessage} = require("../../utils");
+const {sendSuccessMessage, sendFailMessage} = require("../../utils");
 
 class PersonalController {
   getPersonalData(req, res) {
-    UserModel.findOne({_id: "62cf93bec319d67226d3e370"})
-      .populate("details")
-      .populate("servers")
-      .exec((err, data) => {
-        if (err) {
-          res.send(sendFailMessage("fail", {err: err}));
-        }
-        if (data) {
-          res.send(sendSuccessMessage("Success", {personal: data}));
-        }
-      });
+    try {
+      UserModel.findOne({_id: req.query.id})
+        .populate("details")
+        .populate("servers")
+        .exec((err, data) => {
+          if (err) {
+            res.send(sendFailMessage("fail", {err: err}));
+          }
+          if (data) {
+            res.send(sendSuccessMessage("Success", {personal: data}));
+          }
+        });
+    } catch (err) {
+      res.status(201).send(sendFailMessage("failed occurred", err));
+    }
   }
 }
 
