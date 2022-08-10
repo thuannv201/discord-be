8cd2b4786ecaf99e9eac095881575155e388ed78
hochv <hoc17112001@gmail.com>
2022-08-10 14:34:56 +0700

    oke

--- src/controller/user/personal.js
+++ src/controller/user/personal.js
@@ -1,21 +1,26 @@
 const UserModel = require("../../models/user/userModels");
 const ServerModel = require("../../models/servers/server");
-const {sendSuccessMessage, sendFailMessage} = require("../../utils");
+const { sendSuccessMessage, sendFailMessage } = require("../../utils");
 
 class PersonalController {
   getPersonalData(req, res) {
     try {
-      UserModel.findOne({_id: req.query.id})
-        .populate("details")
-        .populate("servers")
-        .exec((err, data) => {
-          if (err) {
-            res.send(sendFailMessage("fail", {err: err}));
-          }
-          if (data) {
-            res.send(sendSuccessMessage("Success", {personal: data}));
-          }
-        });
+      if (req?.query?.id) {
+        UserModel.findOne({ _id: req.query.id })
+          .populate("details")
+          .populate("servers")
+          .exec((err, data) => {
+            if (err) {
+              res.send(sendFailMessage("fail", { err: err }));
+            }
+            if (data) {
+              res.send(sendSuccessMessage("Success", { personal: data }));
+            }
+          });
+      }
+      else{
+        res.send(sendFailMessage("id is required"));
+      }
     } catch (err) {
       res.status(201).send(sendFailMessage("failed occurred", err));
     }
