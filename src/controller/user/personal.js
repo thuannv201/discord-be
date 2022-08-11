eea2a135aa1f92ce8ce60e6e2c704a656ebc9912
hochv <hoc17112001@gmail.com>
2022-08-11 16:34:23 +0700

    get list special

--- src/controller/user/personal.js
+++ src/controller/user/personal.js
@@ -1,6 +1,7 @@
 const UserModel = require("../../models/user/userModels");
 const UserRequest = require("../../models/user/requestUser");
 const {sendSuccessMessage, sendFailMessage} = require("../../utils");
+const UserSpecial = require("../../models/user/userSpecial");
 
 class PersonalController {
   getPersonalData(req, res) {
@@ -69,6 +70,20 @@ class PersonalController {
 
   getListSpecialUsers(req, res) {
     try {
+      UserSpecial.find({id:req.query.id}).populate({ 
+        path: 'specialList',
+        populate: {
+          path: 'id',
+          model: 'Users',
+        } 
+     }).exec((err,data)=>{
+      if(data){
+        res.send(sendSuccessMessage("find success",{data:data}))
+      }
+      if(err){
+        res.status(201).send(sendSuccessMessage("find success",{err:err}))
+      }
+     })
     } catch (e) {}
   }
 
--- src/routes/user/personal.js
+++ src/routes/user/personal.js
@@ -4,6 +4,7 @@ const router = express.Router();
 router.get("/special", PesonalControllers.getListSpecialUsers);
 router.get("/requestReceived", PesonalControllers.getReceivedListRequest);
 router.get("/requestOwn", PesonalControllers.getOwnListRequest);
+router.get("/listSpecialUsers", PesonalControllers.getListSpecialUsers);
 router.get("/", PesonalControllers.getPersonalData);
 
 module.exports = router;
