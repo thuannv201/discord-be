0503d9824971bce36542329100b1a15db6e648be
ThuanNV (OMI.BU3) <thuannv@ominext.com>
2023-05-04 15:15:42 +0700

    ok

--- src/server.ts
+++ src/server.ts
@@ -1,68 +1,67 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import { Server } from "socket.io";
 import {
-  ClientToServerEvents,
-  InterServerEvents,
-  ServerToClientEvents,
-  SocketData,
+    ClientToServerEvents,
+    InterServerEvents,
+    ServerToClientEvents,
+    SocketData,
 } from "./payload/SocketIoPayload";
-const app = express();
 
-const io = new Server<
-  ClientToServerEvents,
-  ServerToClientEvents,
-  InterServerEvents,
-  SocketData
->();
-
+const PORT = process.env.PORT || 1280;
+const app: Express = express();
 app.use(
-  // yarn add cors
-  cors({
-    origin: true,
-    credentials: true,
-    preflightContinue: false,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
-
-app.options("*", cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+    cors({
+        origin: true,
+        credentials: true,
+        preflightContinue: false,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    }),
+    express.json(),
+    express.urlencoded({ extended: true })
+).options("*", cors());
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/chat.html");
+    res.sendFile(__dirname + "/chat.html");
 });
+
+// Socket.IO server
+const io = new Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+>();
 
 io.on("connection", (socket) => {
-  socket.on(
-    "chat_message",
-    (content, attachments, conversationId, authorId) => {
-      if (conversationId) {
-        console.log("conversationId", conversationId);
-      }
-    }
-  );
+    socket.on(
+        "chat_message",
+        (content, attachments, conversationId, authorId) => {
+            if (conversationId) {
+                console.log("conversationId", conversationId);
+            }
+        }
+    );
 
-  socket.on("join_conversation", (conversationId) => {
-    socket.join(conversationId);
-  });
-
-  socket.on("receive_request_friend", (requestor) => {
-    console.log("requestor :", requestor);
-  });
-
-  socket.on("send_request_friend", (requestor, to) => {});
-
-  socket.on("accept_request_user", (requestor, to) => {
-    Promise.all([]).then((values) => {
-      socket.to(to).to(requestor).emit("request_accepted");
+    socket.on("join_conversation", (conversationId) => {
+        socket.join(conversationId);
     });
-  });
+
+    socket.on("receive_request_friend", (requestor) => {
+        console.log("requestor :", requestor);
+    });
+
+    socket.on("send_request_friend", (requestor, to) => {});
+
+    socket.on("accept_request_user", (requestor, to) => {
+        Promise.all([]).then((values) => {
+            socket.to(to).to(requestor).emit("request_accepted");
+        });
+    });
 });
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// start our server
+
+app.listen(PORT, () => {
+    console.log(`Socket.IO server running at http://localhost:${PORT}/`);
 });
- 
-app.listen(7777);
---
+++ src/utils/index.ts
@@ -0,0 +1,36 @@
+function sendFailMessage(
+    message: string = "",
+    other: object = {},
+    status: number | null = null,
+    documentName: string = "element"
+) {
+    switch (status) {
+        case null:
+            return Object.assign(
+                {
+                    status: "fail",
+                    message: message,
+                },
+                other
+            );
+        case 11000:
+            return Object.assign(
+                {
+                    status: "fail",
+                    message: message,
+                    error: `${documentName} already exist`,
+                },
+                other
+            );
+    }
+}
+function sendSuccessMessage(message: string = "", other: object = {}) {
+    return Object.assign(
+        {
+            status: "success",
+            message: message,
+        },
+        other
+    );
+}
+export { sendFailMessage, sendSuccessMessage };
